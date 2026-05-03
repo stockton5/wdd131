af88993f79d4cf701dbeee4d2068f0fa143f52c5
stockton5 <stockton5@byui.edu>
2026-05-02 21:48:56 -0600

    dom

---
+++ dom.js
@@ -0,0 +1,19 @@
+const title = document.querySelector('h1');
+
+console.log(title);
+
+title.textContent = 'Web Page Components';
+
+let topics = document.querySelector('#topics');
+
+//topics.style.color = 'red';
+
+document.getElementById('topics').style.color = 'purple';
+
+let list = document.querySelector('.list');
+
+list.style.border = '3px solid black';
+
+let para = document.querySelector('p');
+
+//para.style.backgroundColor = 'lightblue';
