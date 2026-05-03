edf7f0671a4b6f8992bcf762de12e7bd09b48bde
stockton5 <stockton5@byui.edu>
2026-05-02 22:06:48 -0600

    prove assingment

---
+++ mission/java.js
@@ -0,0 +1,17 @@
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+
+    if (current == 'dark') {
+        document.body.classList.add('dark');
+        logo.src = 'byui-logo-white.png';
+    } else {
+        document.body.classList.remove('dark');
+        logo.src = 'https://www.byui.edu/images/service-sites/map-banner/blue-logo.png';
+    }
+}        
+                    
